3070bafb2be99d90a47ffccaa875679897f8da53
zhaoxiongbin <614619114@qq.com>
2022-10-20 21:27:30 +0800

    day3.5

--- chapter/defer-use.go
+++ chapter/defer-use.go
@@ -2,11 +2,16 @@ package main
 
 import(
 	"fmt"
+	"github.com/lookguy/goWeb/blob/b9fde909d32d4690fc849188a1a0810078204d46/chapter/oob-use.go "
 )
 
 func main(){
 	deferCall()
 	fmt.Println("C")
+
+	s := new (chapter.Student)
+	s.SetName("Zxb")
+	fmt.Println(s.GetName())
 }
 
 func deferCall(){
