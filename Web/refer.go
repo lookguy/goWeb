591ba2137f5187a81e3ee4a1e61f0501e9a917b3
zhaoxiongbin <614619114@qq.com>
2022-10-23 21:51:14 +0800

    net/http 初探

---
+++ Web/refer.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"net/http"
+)
+
+type Refer struct{
+	handler http.Handler
+	refer string
+}
+
+func (this *Refer) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	if r.Referer() == this.refer{
+		this.handler.ServeHTTP(w,r)
+	} else {
+		w.WriteHeader(403)
+	}
+}
+
+func myHandler(w http.ResponseWriter, r  *http.Request){
+	w.Write([]byte("this is handler"))
+}
+
+func hellow(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("hellow"))
+}
+
+func main(){
+	referer := &Refer{
+		handler: http.HandlerFunc(myHandler),
+		refer: "www.zxb.com",
+	}
+	http.HandleFunc("/hello",hellow)
+	http.ListenAndServe(":8080",referer)
+}
