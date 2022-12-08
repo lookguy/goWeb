45c692677b8e84111159d86842594f58990c9991
招雄彬 <zhaoxioingbin@xcdpw.cn>
2022-12-08 22:06:54 +0800

    ServeMux简单应用

--- AWebServer/index.go
+++ AWebServer/index.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"net/http"
-	"log"
 	"fmt"
 )
+// 定义多个处理器
+type handle1 struct{}
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w,"Hello Go Web!")
+func (h1 *handle1) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "hi,handle 1")
+}
+
+type handle2 struct{}
+
+func (h2 *handle2) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "hi,handle 2")
 }
 
 func main(){
-	http.HandleFunc("/hello",helloWorld)
-	if err := http.ListenAndServe(":8080",nil); err != nil {
-		log.Fatal(err)
+	handle1 := handle1{}
+	handle2 := handle2{}
+	mux := http.NewServeMux()
+	
+	server := http.Server{
+		Addr: "0.0.0.0:8085",
+		Handler: mux,
 	}
+	mux.Handle("/handle1", &handle1)
+	mux.Handle("/handle2", &handle2)
+	server.ListenAndServe()
 }
 
